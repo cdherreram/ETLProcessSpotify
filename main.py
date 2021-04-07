3e81b88862a698850f2260c13828aa2138b63a0b
Cristian David Herrera Medina <cdherreram@unal.edu.co>
2021-04-06 20:51:03 -0500

    New update to program in SQLite

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ import sqlite3
 
 DATABASE_LOCATION = "sqlite:///my_played_tracks.sqlite"
 USER_ID = "12171821724"
-TOKEN = "BQBnIFnnyWAV6Mc5Z9UuBLdVsxnAsVpPatwozGuh1NPt15KAGDAbVigU7DWt5713oTk83GkgeKUuwNof6UJxh26b_gUPavub4oCtb370YvL4P8f4Kr1TRD88nQOHGUgrA7BPn7xBEVV72lw7oO81aL6x"
+TOKEN = "BQAeajUkGaFq-aIPwGObLcISBHko6_evrAyiHeIzxx5bO8tL_yQtug3PsrcN7EcP8OaQBwLNtSAl8mkJV8bugODVKQvq5uR9TsFM98giK20YhZ0S5Gu6OQJUya50qs5Nr3EnnaIK5IY1nP6HEX4dd52BlYNgQA"
 
 
 def check_if_valid_data(df: pd.DataFrame) -> bool:
@@ -28,14 +28,14 @@ def check_if_valid_data(df: pd.DataFrame) -> bool:
     if df.isnull().values.any():
         raise Exception("Null valued found")
 
-    yesterday = datetime.datetime.now() - datetime.timedelta(days=10)
-    yesterday = yesterday.replace(hour=0, minute=0, second=0, microsecond=0)
-
-    timestamps = df["timestamp"].tolist()
-    for timestamp in timestamps:
-        if datetime.datetime.strptime(timestamp, '%Y-%m-%d') != yesterday:
-            #raise Exception("At least one of the returned songs does not come from within the last 24 hours")
-            pass
+    # yesterday = datetime.datetime.now() - datetime.timedelta(days=1)
+    # yesterday = yesterday.replace(hour=0, minute=0, second=0, microsecond=0)
+    #
+    # timestamps = df["timestamp"].tolist()
+    # for timestamp in timestamps:
+    #     if datetime.datetime.strptime(timestamp, '%Y-%m-%d') != yesterday:
+    #         #raise Exception("At least one of the returned songs does not come from within the last 24 hours")
+    #         pass
 
     return True
 
@@ -49,11 +49,11 @@ if __name__ == "__main__":
     }
 
     today = datetime.datetime.now()
-    yesterday = today - datetime.timedelta(days=4)
+    yesterday = today - datetime.timedelta(days=60)
     yesterday_unix_timestamp = int(yesterday.timestamp( )) * 1000
 
     try:
-        r = requests.get("https://api.spotify.com/v1/me/player/recently-played?after={time}&limit=5".format(
+        r = requests.get("https://api.spotify.com/v1/me/player/recently-played?after={time}&limit=50".format(
             time=yesterday_unix_timestamp), headers=headers)
         r.raise_for_status()
     except requests.HTTPError as e:
@@ -78,9 +78,35 @@ if __name__ == "__main__":
         "timestamp": timestamps
     }
 
-    songs_df = pd.DataFrame(song_dict, columns=["song_name", "artist_name", "played_at", "timestamp"])
+    song_df = pd.DataFrame(song_dict, columns=["song_name", "artist_name", "played_at", "timestamp"])
 
     # Validate
-    if check_if_valid_data(songs_df):
+    if check_if_valid_data(song_df):
         print("Data valid, proceed to Load stage")
-        print(songs_df)
+        print(song_df)
+
+    #Load
+    engine = sqlalchemy.create_engine(DATABASE_LOCATION)
+    conn = sqlite3.connect('my_played_tracks.sqlite')
+    cursor = conn.cursor()
+
+    sql_query = """
+    CREATE TABLE IF NOT EXISTS my_played_tracks(
+        song_name VARCHAR(200),
+        artist_name VARCHAR(200),
+        played_at VARCHAR(200),
+        timestamp VARCHAR(200),
+        CONSTRAINT primary_key_constraint PRIMARY KEY (played_at)
+        )
+    """
+
+    cursor.execute(sql_query)
+    print("Opened database successfully")
+
+    try:
+        song_df.to_sql("my_played_tracks", engine, index=False, if_exists='append')
+    except:
+        print("Data already exists in the database")
+
+    conn.close()
+    print("Close database successfully")
